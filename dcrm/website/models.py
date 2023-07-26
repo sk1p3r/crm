c8815fc394ccdaad36315b6e38ff40623da6c89a
sk1p3r <annanlusige@gmail.com>
2023-07-26 16:21:01 +0300

    finally

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,17 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    Last_name =models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=10)
+    zipcode = models.CharField(max_length=20)
+
+    def __str__(self):
+        return (f'{self.first_name} {self.Last_name}')
--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
    # Login page is appended on home, it can be created separate if you want
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.customer_record, name='record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
 ]
